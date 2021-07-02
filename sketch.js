0753e51f4a1ff78aa13b232f69335e75c6d0343d
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2021-07-02 17:24:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Composites = Matter.Composites;
+const Composite = Matter.Composite;
 
 var engine, world;
 var ground, crazyBall,basketBall,box1,platform;
@@ -16,9 +16,9 @@ function setup(){
     }
 
     ground = Bodies.rectangle(200,590,600,20,static_options);
-    World.add(world,ground);
+    Composite.add(world,ground);
     platform = Bodies.rectangle(100,520,170,120,static_options);
-    World.add(world,platform);
+    Composite.add(world,platform);
 
     var bb_options ={
         restitution: 0.5,
@@ -29,26 +29,31 @@ function setup(){
     }
 
     basketBall = Bodies.circle(100,100,30, bb_options);
-    World.add(world,basketBall);
+    Composite.add(world,basketBall);
     crazyBall = Bodies.circle(190,100,15, cb_options);
-    World.add(world,crazyBall);
+    Composite.add(world,crazyBall);
     //box1 =  Bodies.rectangle(200,50,50,50);
-    //World.add(world,box1);
+    //Composite.add(world,box1);
     console.log(ground);
 }
 
 function draw(){
     background(0);
     Engine.update(engine);
+    
     rectMode(CENTER);
     fill("brown");
-    
     rect(ground.position.x,ground.position.y,600,20);
-    fill("green");
     rect(platform.position.x,platform.position.y,170,120);
+    
     fill("yellow");
-    rect(box1.position.x,box1.position.y,50,50);
     ellipseMode(RADIUS)
     ellipse(crazyBall.position.x, crazyBall.position.y, 15, 15);
     ellipse(basketBall.position.x, basketBall.position.y, 30, 30);
+    
+    //box1 display
+    //fill("green");
+    //rect(box1.position.x,box1.position.y,50,50);
+
+    
 }
